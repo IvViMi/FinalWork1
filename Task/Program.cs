ede3740907c275f894e95627d3dc936ddc8f734e
Viataly <q08642@yandex.ru>
2022-08-09 09:40:33 +0300

    add method

--- Task/Program.cs
+++ Task/Program.cs
@@ -13,5 +13,19 @@ void PrintArray(string[] arr)
     }
     Console.WriteLine();
 }
-
+void RE_arr(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        string element = arr[i];
+        if (element.Length <= 3)
+        {
+            arr[i] = element;
+    }
+        else arr[i] = string.Empty;
+    }
+}
+PrintArray(array);
+RE_arr(array);
+Console.Write("Итог: ");
 PrintArray(array);

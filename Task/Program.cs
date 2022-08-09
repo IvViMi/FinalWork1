cbf0f2e0021ab65de2bbecda04b2938183f5ad5f
Viataly <q08642@yandex.ru>
2022-08-09 09:37:19 +0300

    add array

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+длина которых меньше либо равна 3 символа.
+*/
+Console.Clear();
+string[] array = new string[] { "qqq", "wwww", "1234", "&", "%%%" };
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(array);
